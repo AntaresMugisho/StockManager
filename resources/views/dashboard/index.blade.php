513ded0597dd944240374a952b7e845a8cc72451
AntaresMugisho <antaresmugisho@gmail.com>
2023-04-07 23:41:44 +0200

    Use image instead of icon just for test

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,12 +1,35 @@
 
 <x-dashboard-layout>
+
+
+    <div class="progress-card">
+        <div class="head">
+            <span class="iconify" data-icon="ic:outline-auto-graph" data-inline="false"></span>
+        </div>
+        <div class="box">
+            <svg>
+                <circle cx="75px" cy="75px" r="60px"></circle>
+                <circle cx="75px" cy="75px" r="60px"></circle>
+                <defs>
+                    <linearGradient id="linear" x1="0%" y1="0%" x2="0%" y2="0%">
+                        <stop offset="0" stop-color="#ffffff"></stop>
+                        <stop offset="1" stop-color="#000000"></stop>
+                    </linearGradient>
+                </defs>
+            </svg>
+            <div class="value"><span>20%</span></div>
+        </div>
+        <div class="label">
+            <span>Articles actifs</span>
+        </div>
+    </div>
     
-    <div class="gauge">
+    {{-- <div class="gauge">
         <div class="gauge-body">
             <div class="gauge-fill" id="gauge_fill"></div>
             <div class="gauge-cover" id="gauge_cover">0%</div>
         </div>
-    </div>
+    </div> --}}
 
     <script>
         const gaugeElement = document.querySelector(".gauge");
