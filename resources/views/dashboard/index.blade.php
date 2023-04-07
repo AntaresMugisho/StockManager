75cd5ffa36567859d2bb7ebedfba55e462d8e0c1
AntaresMugisho <antaresmugisho@gmail.com>
2023-04-07 22:04:56 +0200

    Dashboard layout

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,32 @@
+
+<x-dashboard-layout>
+    <div class="gauge">
+        <div class="gauge-body">
+            <div class="gauge-fill" id="gauge_fill"></div>
+            <div class="gauge-cover" id="gauge_cover">0%</div>
+        </div>
+    </div>
+    
+    <div class="gauge">
+        <div class="gauge-body">
+            <div class="gauge-fill" id="gauge_fill"></div>
+            <div class="gauge-cover" id="gauge_cover">0%</div>
+        </div>
+    </div>
+
+    <script>
+        const gaugeElement = document.querySelector(".gauge");
+
+        function setGaugeValue(gauge, value){
+            if (value < 0 || value > 1){
+                return
+            }
+
+            gauge.querySelector(".gauge-fill").style.transform = `rotate(${value/2}turn)`
+            gauge.querySelector(".gauge-cover").textContent = `${value*100}%`
+        }
+
+        setGaugeValue(gaugeElement, 0.75)
+    </script>
+</x-dashboard-layout>
+
---
+++ resources/views/dashboard/layout/navigation.blade.php
@@ -0,0 +1,13 @@
+
+
+<nav>
+    <ul>
+        <li><a href="#">Art</a></li>
+        <li><a href="#">Accueil</a></li>
+        <li><a href="#">Stock</a></li>
+        <li><a href="#">Commandes</a></li>
+        <li><a href="#">Factures</a></li>
+        <li><a href="#">Imprimables</a></li>
+        <li><a href="#">Paramètres</a></li>
+    </ul>
+</nav>
