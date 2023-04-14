dc16af59ec2ab668ceb19244a08ba4d5ead03ffe
AntaresMugisho <antaresmugisho@gmail.com>
2023-04-14 20:49:53 +0200

    Stock page design done

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,68 +1,69 @@
 
 <x-dashboard-layout>
 
-    <div class="resume-container">
-        <div class="action-bar">
-            <ul class="action-buttons-container">
-                <li>
-                    <button href="#" class="action-button">Ajouter</button>
-                </li>
-                <li>
-                    <button href="#" class="action-button">Commander</button>
-                </li>
-                <li>
-                    <button href="#" class="action-button">Book</button>
-                </li>
-                <li>
-                    <button href="#" class="action-button">Partenaires</button>
-                </li>
+    <div class="home-container">
+        <div class="resume-container">
+            <div class="action-bar">
+                <ul class="action-buttons-container">
+                    <li>
+                        <button href="#" class="action-button">Ajouter</button>
+                    </li>
+                    <li>
+                        <button href="#" class="action-button">Commander</button>
+                    </li>
+                    <li>
+                        <button href="#" class="action-button">Book</button>
+                    </li>
+                    <li>
+                        <button href="#" class="action-button">Partenaires</button>
+                    </li>
+                </ul>
+            </div>
+        
+            <div class="progress-card-container">
+                <div class="progress-card">
+                    <i class="progress-card-icon fa-solid fa-solid fa-chart-line"></i>
+                    <h4 class="progress-card-label">Articles actifs</h4>
+                    @include('components.circular_progressbar')
+                </div>
+                <div class="progress-card">
+                    <i class="progress-card-icon fa-solid fa-cubes-stacked"></i>
+                    <h4 class="progress-card-label">Volume du stock</h4>
+                    @include('components.circular_progressbar')
+                </div>
+                <div class="progress-card">
+                    <i class="progress-card-icon fa-solid fa-file-invoice-dollar"></i>
+                    <h4 class="progress-card-label">Valeur du stock</h4>
+                    @include('components.circular_progressbar')
+                </div>
+            </div>
+            <div class="gauge-container">
+                <div class="gauge">
+                    <div class="gauge-body">
+                        <div class="gauge-fill" id="gauge_fill"></div>
+                        <div class="gauge-cover" id="gauge_cover">0%</div>
+                    </div>
+                    <p class="gauge-label">Articles critiques</p>
+                </div>
+                <div class="gauge">
+                    <div class="gauge-body">
+                        <div class="gauge-fill" id="gauge_fill"></div>
+                        <div class="gauge-cover" id="gauge_cover">0%</div>
+                    </div>
+                    <p class="gauge-label">Articles commandés</p>
+                </div>
+            </div>
+        </div>
+        <div class="financial-review">
+            <h4>Fiances</h4>
+            <ul>
+                <li><span class="small">Capital initial</span> $ 1500,00</li>
+                <li><span class="small">Valeur des commandes</span> $ 1500,00</li>
+                <li><span class="small">Valeur de ventes</span> $ 1500,00</li>
+                <li><span class="small">Créances</span> $ 1500,00</li>
+                <li><span class="small">Caisse</span> $ 1500,00</li>
             </ul>
         </div>
-        
-        <div class="progress-card-container">
-            <div class="progress-card">
-                <i class="progress-card-icon fa-solid fa-solid fa-chart-line"></i>
-                <h4 class="progress-card-label">Articles actifs</h4>
-                @include('components.circular_progressbar')
-            </div>
-            <div class="progress-card">
-                <i class="progress-card-icon fa-solid fa-cubes-stacked"></i>
-                <h4 class="progress-card-label">Volume du stock</h4>
-                @include('components.circular_progressbar')
-            </div>
-            <div class="progress-card">
-                <i class="progress-card-icon fa-solid fa-file-invoice-dollar"></i>
-                <h4 class="progress-card-label">Valeur du stock</h4>
-                @include('components.circular_progressbar')
-            </div>
-        </div>
-        <div class="gauge-container">
-            <div class="gauge">
-                <div class="gauge-body">
-                    <div class="gauge-fill" id="gauge_fill"></div>
-                    <div class="gauge-cover" id="gauge_cover">0%</div>
-                </div>
-                <p class="gauge-label">Articles critiques</p>
-            </div>
-            <div class="gauge">
-                <div class="gauge-body">
-                    <div class="gauge-fill" id="gauge_fill"></div>
-                    <div class="gauge-cover" id="gauge_cover">0%</div>
-                </div>
-                <p class="gauge-label">Articles commandés</p>
-            </div>
-        </div>
-    </div>
-
-    <div class="financial-review">
-        <h4>Fiances</h4>
-        <ul>
-            <li><span class="small">Capital initial</span> $ 1500,00</li>
-            <li><span class="small">Valeur des commandes</span> $ 1500,00</li>
-            <li><span class="small">Valeur de ventes</span> $ 1500,00</li>
-            <li><span class="small">Créances</span> $ 1500,00</li>
-            <li><span class="small">Caisse</span> $ 1500,00</li>
-        </ul>
     </div>
 
     <script>
--- resources/views/dashboard/stock.blade.php
+++ resources/views/dashboard/stock.blade.php
@@ -1,55 +1,136 @@
 <x-dashboard-layout>
 
-    <x-slot name="header">
-        <h2 class="title">Stock</h2>
-        <p class="description">Un oeil sur l'état des articles en stock</p>
-    </x-slot>
+    <div class="stock-container">
+        <div class="dashboard-heading">
+            <h2 class="dashboard-heading-title">Suivie du stock</h2>
+            <p class="dashboard-heading-description">Un oeil sur l'état des articles en stock</p>
+        </div>
 
-    <div class="content">
-        <table>
-            <thead>
-                <tr>
-                    <th>N°</th>
-                    <th>ID</th>
-                    <th>Nom</th>
-                    <th>Description</th>
-                    <th>Prix d'achat unitaire</th>
-                    <th>Prix de vente unitaire</th>
-                    <th>Stock</th>
-                    <th>Minimum</th>
-                    <th>Valeur du stock</th>
-                    <th>Bénéfice attendu</th>
-                    <th>Actif</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>ART-0001</td>
-                    <td>T-shirt Blanc M</td>
-                    <td>Imprimable, 100% coton</td>
-                    <td>2</td>
-                    <td>3</td>
-                    <td>128</td>
-                    <td>12</td>
-                    <td>384</td>
-                    <td>128</td>
-                    <td>Oui</td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>ART-0002</td>
-                    <td>T-shirt Blanc M</td>
-                    <td>Imprimable, 100% coton</td>
-                    <td>2</td>
-                    <td>3</td>
-                    <td>128</td>
-                    <td>12</td>
-                    <td>384</td>
-                    <td>128</td>
-                    <td>Oui</td>
-                </tr>
-            </tbody>
-        </table>
+        <div class="table-container">
+            <table>
+                <thead>
+                    <tr>
+                        <th>N°</th>
+                        <th>ID</th>
+                        <th>Nom</th>
+                        <th>Description</th>
+                        <th>PAU</th>
+                        <th>PVU</th>
+                        <th>Stock</th>
+                        <th>Minimum</th>
+                        <th>Valeur du stock</th>
+                        <th>Bénéfice attendu</th>
+                        <th>Actif</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>ART-0001</td>
+                        <td>T-shirt Blanc S</td>
+                        <td>Imprimable, 100% coton</td>
+                        <td class="currency">2 $</td>
+                        <td class="currency">3 $</td>
+                        <td>128</td>
+                        <td>12</td>
+                        <td class="currency">384 $</td>
+                        <td class="currency">128 $</td>
+                        <td>Oui</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>ART-0001</td>
+                        <td>T-shirt Blanc S</td>
+                        <td>Imprimable, 100% coton</td>
+                        <td class="currency">2 $</td>
+                        <td class="currency">3 $</td>
+                        <td>128</td>
+                        <td>12</td>
+                        <td class="currency">384 $</td>
+                        <td class="currency">128 $</td>
+                        <td>Oui</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>ART-0001</td>
+                        <td>T-shirt Blanc S</td>
+                        <td>Imprimable, 100% coton</td>
+                        <td class="currency">2 $</td>
+                        <td class="currency">3 $</td>
+                        <td>128</td>
+                        <td>12</td>
+                        <td class="currency">384 $</td>
+                        <td class="currency">128 $</td>
+                        <td>Oui</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>ART-0001</td>
+                        <td>T-shirt Blanc S</td>
+                        <td>Imprimable, 100% coton</td>
+                        <td class="currency">2 $</td>
+                        <td class="currency">3 $</td>
+                        <td>128</td>
+                        <td>12</td>
+                        <td class="currency">384 $</td>
+                        <td class="currency">128 $</td>
+                        <td>Oui</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>ART-0001</td>
+                        <td>T-shirt Blanc S</td>
+                        <td>Imprimable, 100% coton</td>
+                        <td class="currency">2 $</td>
+                        <td class="currency">3 $</td>
+                        <td>128</td>
+                        <td>12</td>
+                        <td class="currency">384 $</td>
+                        <td class="currency">128 $</td>
+                        <td>Oui</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>ART-0001</td>
+                        <td>T-shirt Blanc S</td>
+                        <td>Imprimable, 100% coton</td>
+                        <td class="currency">2 $</td>
+                        <td class="currency">3 $</td>
+                        <td>128</td>
+                        <td>12</td>
+                        <td class="currency">384 $</td>
+                        <td class="currency">128 $</td>
+                        <td>Oui</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>ART-0001</td>
+                        <td>Article avec un nom plus long</td>
+                        <td>Imprimable, 100% coton</td>
+                        <td class="currency">2 $</td>
+                        <td class="currency">3 $</td>
+                        <td>128</td>
+                        <td>12</td>
+                        <td class="currency">384 $</td>
+                        <td class="currency">128 $</td>
+                        <td>Oui</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>ART-0001</td>
+                        <td>T-shirt Blanc S</td>
+                        <td>Imprimable, 100% coton</td>
+                        <td class="currency">2 $</td>
+                        <td class="currency">3 $</td>
+                        <td>128</td>
+                        <td>12</td>
+                        <td class="currency">384 $</td>
+                        <td class="currency">128 $</td>
+                        <td>Oui</td>
+                    </tr>
+          
+                </tbody>
+            </table>
+        </div>
     </div>
 </x-dashboard-layout>
