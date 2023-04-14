e78280bca340a2fd18d0827e619fb89ba0fe39ef
AntaresMugisho <antaresmugisho@gmail.com>
2023-04-14 19:34:44 +0200

    Update dashboard layout to disable scrolling on large screens

--- resources/views/dashboard/layout/navigation.blade.php
+++ resources/views/dashboard/layout/navigation.blade.php
@@ -3,12 +3,12 @@
 <nav class="dashboard-navigation">
     <ul>
         <li>
-            {{-- <a href="{{ route('dashboard.index') }}" class="nav-link">@include("components.application-logo")</a> --}}
+            <a href="{{ route('dashboard.index') }}" class="nav-link">@include("components.application-logo")</a>
         </li>
     </ul>
     <ul>
-        {{-- <li><a href="{{route("dashboard.index")}}" class="nav-link nav-link-active"><i class="bi bi-house-fill"></i> Accueil</a></li> --}}
-        {{-- <li><a href="{{route("dashboard.stock")}}" class="nav-link"><i class="fa-solid fa-cubes"></i></i>Stock</a></li> --}}
+        <li><a href="{{route("dashboard.index")}}" class="nav-link nav-link-active"><i class="bi bi-house-fill"></i> Accueil</a></li>
+        <li><a href="{{route("dashboard.stock")}}" class="nav-link"><i class="fa-solid fa-cubes"></i></i>Stock</a></li>
         <li><a href="#" class="nav-link"><i class="fa-solid fa-cart-arrow-down"></i> Commandes</a></li>
         <li><a href="#" class="nav-link"><i class="fa-solid fa-file-invoice-dollar"></i> Factures</a></li>
         <li><a href="#" class="nav-link"><i class="fa-solid fa-print"></i> Imprimables</a></li>
