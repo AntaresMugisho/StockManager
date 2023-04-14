e871d77122c0a547368705e1b690019edf34c4ba
AntaresMugisho <antaresmugisho@gmail.com>
2023-04-14 21:10:00 +0200

    Create Article model and migration

---
+++ database/migrations/2023_04_14_190512_create_articles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string("code")->unique();
+            $table->string("name");
+            $table->mediumText("description")->nullable();
+            $table->integer("unit_purchase_price");
+            $table->integer("unit_selling_price");
+            $table->integer("stock");
+            $table->integer("minimum_stock");
+            $table->integer("stock_value");
+            $table->integer("benefit");
+            $table->boolean("active");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
