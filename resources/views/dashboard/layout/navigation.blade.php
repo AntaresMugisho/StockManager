d98aebc0a2f00895554759d6a7cfc753b78d519f
AntaresMugisho <antaresmugisho@gmail.com>
2023-04-18 15:44:12 +0200

    Change dashboard navbar to indicate active menu

--- resources/views/dashboard/layout/navigation.blade.php
+++ resources/views/dashboard/layout/navigation.blade.php
@@ -6,11 +6,15 @@
             <a href="{{ route('dashboard.index') }}" class="nav-link">@include("components.application-logo")</a>
         </li>
     </ul>
+    @php
+        $route = request()->route()->getName();
+    @endphp
+    
     <ul>
-        <li><a href="{{ route("dashboard.index") }}" class="nav-link nav-link-active"><i class="bi bi-house-fill"></i> Accueil</a></li>
-        <li><a href="{{ route("article.index") }}" class="nav-link"><i class="fa-solid fa-cubes"></i></i>Stock</a></li>
-        <li><a href="{{ route("dashboard.orders") }}" class="nav-link"><i class="fa-solid fa-cart-arrow-down"></i> Commandes</a></li>
-        <li><a href="#" class="nav-link"><i class="fa-solid fa-file-invoice-dollar"></i> Factures</a></li>
+        <li><a href="{{ route("dashboard.index") }}" class="nav-link {{ ($route == "dashboard.index") ? "nav-link-active" : null }}"><i class="bi bi-house-fill"></i> Accueil</a></li>
+        <li><a href="{{ route("article.index") }}" class="nav-link {{ str_contains($route, "article.") ? "nav-link-active" : null }}"><i class="fa-solid fa-cubes"></i></i>Stock</a></li>
+        <li><a href="{{ route("order.index") }}" class="nav-link {{ str_contains($route, "order.") ? "nav-link-active" : null }}"><i class="fa-solid fa-cart-arrow-down"></i> Commandes</a></li>
+        <li><a href="#" class="nav-link {{ str_contains($route, "invoice.") ? "nav-link-active" : null }}"><i class="fa-solid fa-file-invoice-dollar"></i> Factures</a></li>
         <li><a href="#" class="nav-link"><i class="fa-solid fa-print"></i> Imprimables</a></li>
     </ul>
     <ul>
